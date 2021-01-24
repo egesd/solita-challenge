329f651e9eb0a5644235daf397f11ea698018443
egesd <eliastsky@gmail.com>
2021-01-24 17:16:56 +0200

    Remove duplicated directory

--- back/populateDb.js
+++
@@ -1,9 +0,0 @@
-const fs = require("fs");
-
-function readData() {
-    let rawdata = fs.readFileSync("./data/names.json");
-    let names = JSON.parse(rawdata);
-    console.log(names);
-}
-
-module.exports = { readData }
